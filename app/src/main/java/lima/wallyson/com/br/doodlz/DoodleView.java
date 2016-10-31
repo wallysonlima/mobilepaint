7fd170cff0121a9aee43b7fb003c1e929dd014b0
WallysonNunes <wallyson.n.a.lima@gmail.com>
2016-10-31 10:58:38 -0200

    Created DoodleView class

---
+++ app/src/main/java/lima/wallyson/com/br/doodlz/DoodleView.java
@@ -0,0 +1,15 @@
+package lima.wallyson.com.br.doodlz;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by wally on 31/10/16.
+ */
+public class DoodleView extends View {
+
+    public DoodleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}

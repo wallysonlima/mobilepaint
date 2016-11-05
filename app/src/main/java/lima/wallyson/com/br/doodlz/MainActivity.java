a57281eed3d6458a1dd2c421dbfac895006746f7
WallysonNunes <wallyson.n.a.lima@gmail.com>
2016-11-05 17:45:08 -0200

    Updated MainActivity and MainActivityFragment

--- app/src/main/java/lima/wallyson/com/br/doodlz/MainActivity.java
+++ app/src/main/java/lima/wallyson/com/br/doodlz/MainActivity.java
@@ -1,13 +1,10 @@
 package lima.wallyson.com.br.doodlz;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +15,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // determine screen size
+        int screenSize =
+                getResources().getConfiguration().screenLayout &
+                        Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        // use landscape for extra large tablets; otherwise, use portrait
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE )
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            );
+        else
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            );
     }
 
 
--- app/src/main/java/lima/wallyson/com/br/doodlz/MainActivityFragment.java
+++ app/src/main/java/lima/wallyson/com/br/doodlz/MainActivityFragment.java
@@ -5,18 +5,139 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private DoodleView doodleView; // handles touch events and draws
+    private float acceleration, currentAcceleration, lastAcceleration;
+    private boolean dialogOnScreen = false;
 
-    public MainActivityFragment() {
+    // value used to determine whether user shook the device to erase
+    private static final int ACCELERATION_THRESHOLD = 100000;
+
+    // used to identify the request for using external storage, which
+    // the save image feature needs
+    private static final int SAVE_IMAGE_PERMISSION_REQUEST_ONCE = 1;
+
+
+    public View MainActivityFragment() {
     }
 
+    // called when Fragment's view needs to be created
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view =
+                inflater.inflate(R.layout.fragment_main, container, false);
+
+        setHasOptionsMenu(true); // this fragment has menu items to display
+
+        // get reference to the DoodleView
+        doodleView = (DoodleView) view.findViewById(R.id.doodleView);
+
+        // initialize acceleration values
+        acceleration = 0.00f;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+        return view;
+    }
+
+    //Override
+    public void onResume() {
+        super.onResume();
+        enableAcceLerometerListening(); // listen for shake event
+    }
+
+    // enable listening for accelerometer events
+    private void enableAccelerometerListening() {
+        // get the SensorManager
+        SensorManager sensorManager =
+                (SensorManager) getActivity().getSystemService(
+                        Context.SENSOR_SERVICE
+                );
+
+        // register to listen for accelerometer events
+        SensorManager.registerListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    // stop listening for accelerometer events
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableAcceLerometerListening(); // stop listening for shake
+    }
+
+    // disable listening for accelerometer events
+    private void disableAccelerometerListening() {
+        // get the SensorManager
+        SensorManager sensorManager =
+                (SensorManager) getActivity().getSystemService(
+                        Context.SENSOR_SERVICE
+                );
+
+        // stop listening for accelerometer events
+        sensorManager.unregisterListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+    }
+
+    private final SensorEventListener sensorEventListener =
+            new SensorEventListener() {
+                // use accelerometer to determine whether user shook device
+                @Override
+                public void onSensorChanged(SensorEvent sensorEvent) {
+                    // ensure that other dialogs are not displayed
+                    if (!dialogOnScreen) {
+                        // get x, y, and z values for the SensorEvent
+                        float x = event.values[0];
+                        float y = event.values[1];
+                        float z = event.values[2];
+
+                        // save previous acceleration value
+                        lastAcceleration = currentAcceleration;
+
+                        // calculate the current acceleration
+                        currentAcceleration = x * x + y * y + z * z;
+
+                        // calculate the change in acceleration
+                        acceleration = currentAcceleration *
+                                (currentAcceleration - lastAcceleration);
+
+                        // if the acceleration is above a certain threshold
+                        if (acceleration > ACCELERATION_THRESHOLD)
+                            confirmErase();
+                    }
+                }
+
+                // required method of interface SensorEventListener
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            };
+
+    // confirm whether image should be erased
+    private void confirmErase() {
+        EraseImageDialogFragment fragment = new EraseImageDialogFragment();
+        fragment.show(getFragmentManager(), "erase dialog");
     }
 }

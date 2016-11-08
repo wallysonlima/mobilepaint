a7d9d8c45cb7e577344895bfde0b0b4a5f060619
WallysonNunes <wallyson.n.a.lima@gmail.com>
2016-11-08 11:20:15 -0200

    Updated LineWidthDialogFragment class

--- app/src/main/java/lima/wallyson/com/br/doodlz/LineWidthDialogFragment.java
+++ app/src/main/java/lima/wallyson/com/br/doodlz/LineWidthDialogFragment.java
@@ -1,7 +1,130 @@
+// LineWidthDialogFrament.java
+// Allows user to set the drawing color on the DoodleView
+
 package lima.wallyson.com.br.doodlz;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 /**
  * Created by wally on 05/11/16.
  */
-public class LineWidthDialogFragment {
+
+// class for the Select Line Width dialog
+public class LineWidthDialogFragment extends DialogFragment {
+    private ImageView widthImageView;
+
+    // create an AlertDialog and return it
+    @Override
+    public Dialog onCreateDialog(Bundle bundle) {
+        // create the dialog
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity());
+
+        View lineWidthDialogView =
+                getActivity().getLayoutInflater().inflate(
+                        R.layout.fragment_line_width, null);
+
+        builder.setView(lineWidthDialogView); // add GUI to dialog
+
+        // set the AlertDialog's message
+        builder.setTitle(R.string.title_line_width_dialog);
+
+        // get the ImageView
+        widthImageView = (ImageView) lineWidthDialogView.findViewById(
+                R.id.widthImageView);
+
+        // configure widthSeekBar
+        final DoodleView doodleView = getDoodleFragment().getDoodleView();
+        final SeekBar widthSeekBar = (SeekBar)
+                lineWidthDialogView.findViewById(R.id.widthSeekBar);
+
+        widthSeekBar.setOnSeekBarChangeListener(lineWidthChanged);
+        widthSeekBar.setProgress(doodleView.getLineWidth());
+
+        // add Set Line Width Button
+        builder.setPositiveButton(R.string.button_set_line_width,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        doodleView.setLineWidth(widthSeekBar.getProgress());
+                    }
+                }
+        );
+
+        return builder.create(); // return dialog
+    }
+
+    // return a reference to the MainActivityFragment
+    private MainActivityFragment getDoodleFragment() {
+        return (MainActivityFragment) getFragmentManager().findFragmentById(
+                R.id.doodleFragment);
+    }
+
+    // tell MainActivityFragment that dialog is now displayed
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if ( fragment != null )
+            fragment.setDialogOnScreen(true);
+    }
+
+    // tell MainActivityFrament that dialog is no longer displayed
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if ( fragment != null )
+            fragment.setDialogOnScreen(false);
+    }
+
+    // OnSeekBarChangeListener for the SeekBar in the width dialog
+    private final SeekBar.OnSeekBarChangeListener lineWidthChanged =
+            new SeekBar.OnSeekBarChangeListener() {
+                final Bitmap bitmap = Bitmap.createBitmap(
+                        400, 100, Bitmap.Config.ARGB_8888);
+                final Canvas canvas = new Canvas(bitmap); // draws into bitmap
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    // configure a Paint object for the current SeekBar value
+                    Paint p = new Paint();
+                    p.setColor(
+                            getDoodleFragment().getDoodleView().getDrawingColor());
+                    p.setStrokeCap(Paint.Cap.ROUND);
+                    p.setStrokeWidth(progress);
+
+                    // erase the bitmap and redraw the line
+                    bitmap.eraseColor(
+                            getResources().getColor(android.R.color.transparent,
+                                    getContext().getTheme()));
+
+                    canvas.drawLine(30, 50, 370, 50, p);
+                    widthImageView.setImageBitmap(bitmap);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            };
+
 }

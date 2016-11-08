c06448896550bcd14b286854cc4aea84196eb8b9
WallysonNunes <wallyson.n.a.lima@gmail.com>
2016-11-08 11:32:20 -0200

    Updated EraseImageDialogFragment class

--- app/src/main/java/lima/wallyson/com/br/doodlz/EraseImageDialogFragment.java
+++ app/src/main/java/lima/wallyson/com/br/doodlz/EraseImageDialogFragment.java
@@ -1,7 +1,66 @@
 package lima.wallyson.com.br.doodlz;
 
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
 /**
  * Created by wally on 05/11/16.
  */
-public class EraseImageDialogFragment {
+
+// class for the Erase Image dialog
+public class EraseImageDialogFragment extends DialogFragment {
+    // create an AlertDialog and return it
+    @Override
+    public Dialog onCreateDialog(Bundle bundle) {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity());
+
+        // set the AlertDialog's message
+        builder.setMessage(R.string.message_erase);
+
+        // add Erase Button
+        builder.setPositiveButton(R.string.button_erase,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        getDoodleFragment().getDoodleView().clear(); // clear image
+                    }
+                }
+        );
+
+        // add cancel Button
+        builder.setNegativeButton(android.R.string.cancel, null);
+        return builder.create(); // return dialog
+    }
+
+    // gets a reference to the MainActivityFragment
+    private MainActivityFragment getDoodleFragment() {
+        return (MainActivityFragment) getFragmentManager().findFragmentById(
+                R.id.doodleFragment);
+    }
+
+    // tell MainActivityFragment that dialog is now displayed
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if ( fragment != null )
+            fragment.setDialogOnScreen(true);
+    }
+
+    // tell MainActivityFragment that dialog is no longer displayed
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        MainActivityFragment fragment = getDoodleFragment();
+
+        if ( fragment != null )
+            fragment.setDialogOnScreen(false);
+    }
 }

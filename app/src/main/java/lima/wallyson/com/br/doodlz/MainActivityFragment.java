60135ecda5079a56f14a4c6dc0d9f36b6ef8eb95
WallysonNunes <wallyson.n.a.lima@gmail.com>
2016-11-14 15:31:51 -0200

    Updated MainActivityFragment class

--- app/src/main/java/lima/wallyson/com/br/doodlz/MainActivityFragment.java
+++ app/src/main/java/lima/wallyson/com/br/doodlz/MainActivityFragment.java
@@ -19,8 +19,6 @@ import android.hardware.SensorManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * A placeholder fragment containing a simple view.
